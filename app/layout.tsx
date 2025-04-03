8fe9092b77d74cbff37553e185bb56bfef3807c0
Dane Jackson <danedjackson@yahoo.com>
2025-04-03 12:50:52 -0500

    Customize radix-ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
-import './globals.css'
+import "@radix-ui/themes/styles.css"
+import './css/themeconfig.css'
+import './css/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import { Theme } from '@radix-ui/themes'
+import { Theme, ThemePanel } from '@radix-ui/themes'
 
-import "@radix-ui/themes/styles.css";
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter'
+})
 
 export const metadata: Metadata = {
   title: 'Issue Tracker',
@@ -20,13 +23,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="winter">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" data-theme="winter" className={inter.variable}>
+      <body>
+      <Theme accentColor="violet">
           <NavBar />
           <main className='p-5'>
             {children}
           </main>
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
