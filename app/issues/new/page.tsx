b782d471b5065fc2b1d4d9ca21801b3247c577b0
Dane Jackson <danedjackson@yahoo.com>
2025-04-14 10:20:59 -0500

    Extracted in-line function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -15,6 +15,8 @@ import Spinner from '@/app/components/Spinner'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
+
+
 const NewIssuePage = () => {
   const router = useRouter()
   const { register, control, handleSubmit, formState: {  errors } } = useForm<IssueForm>({
@@ -22,6 +24,17 @@ const NewIssuePage = () => {
   })
   const [error, setError] = useState('')
   const [isSubmitting, setSubmitting] = useState(false)
+
+  const submitForm = handleSubmit(async (data) => {
+    try{
+      setSubmitting(true)
+      await axios.post('/api/issues', data)
+      router.push('/issues')
+    } catch(error) {
+      setError('An unexpected error occurred')
+      setSubmitting(false)
+    } 
+  })
 
   return (
     <div className='max-w-xl space-y-3'>
@@ -31,16 +44,7 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
       <form  
-          onSubmit={handleSubmit(async (data) => {
-            try{
-              setSubmitting(true)
-              await axios.post('/api/issues', data)
-              router.push('/issues')
-            } catch(error) {
-              setError('An unexpected error occurred')
-              setSubmitting(false)
-            } 
-          })}>
+          onSubmit={submitForm}>
           <TextField.Root placeholder='Title' {...register('title')}/>
           <ErrorMessage>{errors.title?.message}</ErrorMessage>
           <Controller 
